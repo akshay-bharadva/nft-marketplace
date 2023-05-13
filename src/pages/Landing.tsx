538c5cb8e032a1e9a1f7f2256942063d76b56f82
Akshay Bharadva <akshaybharadva19@gmail.com>
2023-05-13 11:08:15 +0530

    Footer

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,12 +1,14 @@
 import { Profiler } from "react";
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
+import Footer from "../components/Footer/Footer";
 
 const Landing = () => {
   return (
     <Profiler id="Landing-Page" onRender={() => {}}>
       <Header />
       <Hero />
+      <Footer />
     </Profiler>
   );
 };
