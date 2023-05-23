d2e67eb339486015e1ce1354a6cc6252c010283b
Akshay Bharadva <akshaybharadva19@gmail.com>
2023-05-23 22:42:37 +0530

    responsive ui

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,18 +8,18 @@ const Header = () => {
   return (
     <Profiler id="Header" onRender={() => {}}>
       <header className={`${CLASS_NAME}`}>
-        <div className={`${CLASS_NAME}__container row`}>
-          <div className={`${CLASS_NAME}__menu-container col-4`}>
+        <div className={`${CLASS_NAME}__container container row`}>
+          <div className={`${CLASS_NAME}__menu-container col-4 d-none d-md-flex`}>
             <ul className={`${CLASS_NAME}__menu`}>
               <li>Marketplace</li>
               <li>Creators</li>
               <li>Community</li>
             </ul>
           </div>
-          <div className={`${CLASS_NAME}__title-container col-4`}>
+          <div className={`${CLASS_NAME}__title-container col-12 col-sm-12 col-md-4`}>
             <h3 className={`${CLASS_NAME}__title`}>NFT</h3>
           </div>
-          <div className={`${CLASS_NAME}__wallet-container col-4`}>
+          <div className={`${CLASS_NAME}__wallet-container col-4 d-none d-md-flex`}>
             <Button content="Connect Wallet" isFilled isRounded />
           </div>
         </div>
